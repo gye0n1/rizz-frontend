d660b834469e5bd49257d7f1876c5f1635108569
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2025-03-16 09:34:26 +0900

    commit

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/rizz-frontend/", // GitHub Pages의 repo 이름을 입력
-});
