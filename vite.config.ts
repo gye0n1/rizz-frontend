7e0cf015fd4eebce013e4cc43e44ff60d4a7a9fa
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2025-03-16 09:16:58 +0900

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/rizz-frontend/", // GitHub Pages의 repo 이름을 입력
 });
